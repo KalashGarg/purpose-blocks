299f24983c836a43a5864507c30ec6f41a4d2d2c
Kalash garg <86044442+KalashGarg@users.noreply.github.com>
2023-09-13 22:16:35 +0530

    Add files via upload

---
+++ src/components/Timer.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+
+const Timer = () => {
+
+  return (
+    <div className="timer">
+      Time: 00:00:00
+       </div>
+  );
+};
+
+export default Timer;
